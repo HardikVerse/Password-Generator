2976cf763da8edb6a28d21cbc4bd8756658b22dc
Hardik Gupta <hello.hardikgupta@gmail.com>
2026-05-22 13:13:55 +0530

    Enhance password generator with strength check
    Added password strength evaluation and loop for generating multiple passwords.

--- main.py
+++ main.py
@@ -14,7 +14,14 @@ user_input = int(input("Enter length of password: "))
 if user_input not in range(3, 129):
     print("\nPlease, enter length in between 3 and 128.")
 
+
 else:
+    if user_input < 8:
+        print("Strength: Weak")
+    elif user_input < 14:
+        print("Strenght: Medium")
+    else:
+        print("Strength: Strong")
 
     print("\nPress 1 to Select and 0 to Deselect\n")
 
@@ -25,13 +32,14 @@ else:
 
 
     if not all(x in (0,1) for x in [A_to_Z, a_to_z, numbers, special_word]):
-            print("\nPlease enter digit either 1 or 0.")
+            print("\nPlease enter digit either 1 or 0.\n")
 
         
     elif all(x == 0 for x in [A_to_Z, a_to_z, numbers, special_word]):
             print("\nPlease select atleast one option.")
             
     else:
+
         option = {
             string.ascii_uppercase: A_to_Z,
             string.ascii_lowercase: a_to_z,
@@ -48,5 +56,10 @@ else:
             password = random.choices(character, k=length)
             result = "".join(password)
             return f"\nHere is your password: {result}\n"
-        print(pass_generator(final_string, user_input))
-       
+        while True:
+            print(pass_generator(final_string, user_input))
+            gen_again = input("Generate another password? (y/n):" )
+            if gen_again == "n":
+                break
+            
+
